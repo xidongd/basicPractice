9171b8e03d957667e215fb3236d799c86203a673
xidongd <duxidong1989@hotmail.com>
2016-04-10 14:04:07 +0800

    use for test commit

---
+++ strcmp.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  strcmp.c
+ *
+ *    Description:  realize cmp two strings,cmp every byte, if s1[i]>s2[i] ret=1
+ *     				s1[i]=s2[i] ret=0 s1[i]<s2[i] ret=-1
+ *        Version:  1.0
+ *        Created:  2016年04月10日 13时35分19秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
